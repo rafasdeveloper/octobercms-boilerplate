ba8a9f017b0e873707079a362b43ec88d436e084
Rafael Sanchez <rafael@booug.com>
2020-09-21 20:12:07 -0500

    importing base db.

--- base/docker-entrypoint.sh
+++ base/docker-entrypoint.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -e
 
+mysql -u root -ptoor octobercms_db < /var/www/html/database.sql
+
+cd /var/www/html
+
+composer install
+
+composer update
+
 chmod -R 777 /var/www/html
 
 supervisord -c /etc/supervisord.conf
